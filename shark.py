745d1ef4c064a6468c797d5083a51cac7cdaa858
EngRobot33 <hmdkhsrvee@gmail.com>
2023-01-01 13:35:46 +0330

    [ADD] Interpreter

--- shark.py
+++ shark.py
@@ -34,8 +34,32 @@ class IllegalCharError(Error):
 
 
 class InvalidSyntaxError(Error):
-    def __init__(self, pos_start, pos_end, details=''):
+    def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
+
+
+class RunTimeError(Error):
+    def __init__(self, pos_start, pos_end, details, context):
+        super().__init__(pos_start, pos_end, 'Runtime Error', details)
+        self.context = context
+
+    def __str__(self):
+        result = self.generate_traceback()
+        result += f'{self.error_name}: {self.details}' + '\n'
+        result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end) + '\n'
+        return result
+
+    def generate_traceback(self):
+        result = ''
+        pos = self.pos_start
+        ctx = self.context
+
+        while ctx:
+            result = f'  File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+            pos = ctx.parent_entry_pos
+            ctx = ctx.parent
+
+        return 'Traceback (most recent call last):\n' + result
 
 
 class Position:
@@ -151,6 +175,9 @@ class NumberNode:
     def __init__(self, tok):
         self.tok = tok
 
+        self.pos_start = self.tok.pos_start
+        self.pos_end = self.tok.pos_end
+
     def __str__(self):
         return f'{self.tok}'
 
@@ -161,6 +188,9 @@ class BinOpNode:
         self.op_tok = op_tok
         self.right_node = right_node
 
+        self.pos_start = self.left_node.pos_start
+        self.pos_end = self.right_node.pos_end
+
     def __str__(self):
         return f'({self.left_node}, {self.op_tok}, {self.right_node})'
 
@@ -169,6 +199,9 @@ class UnaryOpNode:
     def __init__(self, op_tok, node):
         self.op_tok = op_tok
         self.node = node
+
+        self.pos_start = self.op_tok.pos_start
+        self.pos_end = node.pos_end
 
     def __str__(self):
         return f'({self.op_tok}, {self.node})'
@@ -206,21 +239,21 @@ class Parser:
     def advance(self, ):
         self.tok_idx += 1
         if self.tok_idx < len(self.tokens):
-            self.current_tok = self.tokens[self.tok_idx]
-        return self.current_tok
+            self.curr_tok = self.tokens[self.tok_idx]
+        return self.curr_tok
 
     def parse(self):
         res = self.expr()
-        if not res.error and self.current_tok.type != TT_EOF:
+        if not res.error and self.curr_tok.type != TT_EOF:
             return res.failure(InvalidSyntaxError(
-                self.current_tok.pos_start, self.current_tok.pos_end,
+                self.curr_tok.pos_start, self.curr_tok.pos_end,
                 "Expected '+', '-', '*' or '/'"
             ))
         return res
 
     def factor(self):
         res = ParseResult()
-        tok = self.current_tok
+        tok = self.curr_tok
 
         if tok.type in (TT_PLUS, TT_MINUS):
             res.register(self.advance())
@@ -241,12 +274,12 @@ class Parser:
 
             if res.error:
                 return res
-            if self.current_tok.type == TT_RPAREN:
+            if self.curr_tok.type == TT_RPAREN:
                 res.register(self.advance())
                 return res.success(expr)
             else:
                 return res.failure(InvalidSyntaxError(
-                    self.current_tok.pos_start, self.current_tok.pos_end,
+                    self.curr_tok.pos_start, self.curr_tok.pos_end,
                     "Expected ')'"
                 ))
 
@@ -268,8 +301,8 @@ class Parser:
         if res.error:
             return res
 
-        while self.current_tok.type in ops:
-            op_tok = self.current_tok
+        while self.curr_tok.type in ops:
+            op_tok = self.curr_tok
             res.register(self.advance())
             right = res.register(func())
 
@@ -279,6 +312,120 @@ class Parser:
             left = BinOpNode(left, op_tok, right)
 
         return res.success(left)
+
+
+class RTResult:
+    def __init__(self):
+        self.value = None
+        self.error = None
+
+    def register(self, res):
+        if res.error: self.error = res.error
+        return res.value
+
+    def success(self, value):
+        self.value = value
+        return self
+
+    def failure(self, error):
+        self.error = error
+        return self
+
+
+class Number:
+    def __init__(self, value):
+        self.value = value
+        self.set_pos()
+        self.set_context()
+
+    def set_pos(self, pos_start=None, pos_end=None):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        return self
+
+    def set_context(self, context=None):
+        self.context = context
+        return self
+
+    def add_to(self, other):
+        if isinstance(other, Number):
+            return Number(self.value + other.value).set_context(self.context), None
+
+    def sub_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value - other.value).set_context(self.context), None
+
+    def mult_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value * other.value).set_context(self.context), None
+
+    def div_by(self, other):
+        if isinstance(other, Number):
+            if other.value == 0:
+                return None, RunTimeError(
+                    other.pos_start, other.pos_end, 'Division by zero', self.context
+                )
+            return Number(self.value / other.value).set_context(self.context), None
+
+    def __str__(self):
+        return f'{self.value}'
+
+
+class Context:
+    def __init__(self, display_name, parent=None, parent_entry_pos=None):
+        self.display_name = display_name
+        self.parent = parent
+        self.parent_entry_pos = parent_entry_pos
+
+
+class Interpreter:
+    def visit(self, node, context):
+        method_name = f'visit_{type(node).__name__}'
+        method = getattr(self, method_name, self.no_visit_method)
+        return method(node, context)
+
+    def no_visit_method(self, node, context):
+        raise Exception(f'No visit_{type(node).__name__} method defined!')
+
+    def visit_NumberNode(self, node, context):
+        return RTResult().success(
+            Number(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end)
+        )
+
+    def visit_BinOpNode(self, node, context):
+        res = RTResult()
+        left = res.register(self.visit(node.left_node, context))
+        if res.error: return res
+        right = res.register(self.visit(node.right_node, context))
+        if res.error: return res
+
+        if node.op_tok.type == TT_PLUS:
+            result, error = left.add_to(right)
+        elif node.op_tok.type == TT_MINUS:
+            result, error = left.sub_by(right)
+        elif node.op_tok.type == TT_MUL:
+            result, error = left.mult_by(right)
+        elif node.op_tok.type == TT_DIV:
+            result, error = left.div_by(right)
+
+        if error:
+            return res.failure(error)
+        else:
+            return res.success(result.set_pos(node.pos_start, node.pos_end))
+
+
+    def visit_UnaryOpNode(self, node, context):
+        res = RTResult()
+        number = res.register(self.visit(node.node, context))
+        if res.error: return res
+
+        if node.op_tok.type == TT_MINUS:
+            number, error = number.mult_by(Number(-1))
+
+        if error:
+            return res.failure(error)
+
+        return res.success(number.set_pos(node.pos_start, node.pos_end))
 
 
 def run(fn, text):
@@ -291,4 +438,11 @@ def run(fn, text):
     parser = Parser(tokens)
     ast = parser.parse()
 
-    return ast.node, ast.error
+    if ast.error:
+        return None, ast.error
+
+    interpreter = Interpreter()
+    context = Context('<program>')
+    result = interpreter.visit(ast.node, context)
+
+    return result.value, result.error
